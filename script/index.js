fe1cacafe70de6a62bbcbbd5942d346189aa0850
Mohsin Abdullah Chowdhury <mohsinabd71@gmail.com>
2025-09-11 10:40:01 +0600

    ongoing

--- script/index.js
+++ script/index.js
@@ -4,6 +4,31 @@ const loadLessons = () =>{
     .then((json) => displayLesson(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    console.log(url);
+    fetch(url)
+    .then(res=>res.json())
+    // .then(data => console.log(data));
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    // wordContainer.innerHTML="";
+
+    words.forEach(word => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <p>Dog</p>
+        `;
+        
+        wordContainer.append(card);
+    });
+
+}
+
 displayLesson= (lessons) =>{
 
     // 1.get the container and empty that
@@ -23,7 +48,7 @@ displayLesson= (lessons) =>{
     // 3
     const newDiv = document.createElement("div")
     newDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no}</button>
+    <button onclick= "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no}</button>
     `
        
     // 4
